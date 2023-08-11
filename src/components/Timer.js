de53c7f8b2b69a169ed6b90c2af03fc138273ce2
Shubham Verma <84174011+Stroller15@users.noreply.github.com>
2023-08-11 18:13:33 +0530

    implemented start, stop , reset button functionality

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,17 +1,45 @@
+import "./timer.css";
 import { useEffect, useState } from "react";
 
 const Timer = () => {
   const [second, setSecond] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    let timerId = setInterval(() => {
-      setSecond(second + 1);
-    }, 1000);
-
+    let timerId;
+    if (isRunning) {
+      timerId = setInterval(() => {
+        setSecond(second + 1);
+      }, 1000);
+    }
     return () => clearInterval(timerId);
   });
 
-  return <div>{second}</div>;
+  // Implemented start button
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+  // Implemented Stop button
+  const handleStop = () => {
+    const stopedSecond = second;
+    setIsRunning(false);
+    setSecond(stopedSecond);
+  };
+  // Implemented Reset button
+  const handleReset = () => {
+    setIsRunning(false);
+    setSecond(0);
+  };
+
+  return (
+    <div className="container">
+      <div className="second">{second}</div>
+
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
 };
 
 export default Timer;
